6cf3da2a090d2f6e36cb6265d0f0fbfe562d8c4b
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-12-08 19:27:05 +0000

    feat: full app that plots maps using google maps

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,42 @@
-// Code goes here!
+import { GOOGLE_API_KEY } from "./constants";
+import axios from "axios";
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+//declare var google: any;
+
+type GoogleGeocodingResponse = {
+    results: { geometry: { location: {lat: number; lng: number } } }[];
+    status: 'OK' | 'ZERO_RESULTS';
+}
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  //Send Address to Google API
+  axios.get<GoogleGeocodingResponse>(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+      enteredAddress
+    )}&key=${GOOGLE_API_KEY}`
+  ).then(res => {
+      if (res.data.status !== 'OK'){
+        throw new Error('Zero Results, try again!');
+      }    
+      const coordinates = res.data.results[0].geometry.location;
+      
+      const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+        center: coordinates,
+        zoom: 13,
+      });
+
+      new google.maps.Marker({position: coordinates, map: map});
+
+  }).catch(err => {
+    alert(err.message);
+    console.log(err);
+  });
+}
+
+form.addEventListener("submit", searchAddressHandler);
